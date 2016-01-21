0697eb7848182963726595b2d02f8ad93df450d7
Kasper Holbek Jensen <khj@autobutler.dk>
2016-01-21 16:28:19 +0100

    base specs

--- spec/cash_spec.rb
+++ spec/cash_spec.rb
@@ -5,7 +5,32 @@ describe Cash do
     expect(Cash::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(false)
+  describe 'configuration' do
+    it 'can instantiate without setup' do
+      expect(Cash.new('EUR', 50)).not_to raise_error
+    end
+
+    it 'fails on convertion without rates' do
+      expect(Cash.new('EUR', 50).convert_to('USD')).to raise_error
+    end
+  end
+
+  describe 'convertion' do
+    let(:fifty_euro) {Cash.new('EUR', 50)}
+
+    before do
+      Cash.convertion_rates('EUR', {
+        'USD' => 1.11,
+        'Bitcoin' => 0.0047
+      })
+    end
+
+    it 'converts to a different currency' do
+      expect(fifty_euro.convert_to('USD')).to be_a Cash
+    end
+
+    it 'fails with a wrong currency' do
+      expect(fifty_euro.convert_to('Sausages')).to raise_error
+    end
   end
 end
