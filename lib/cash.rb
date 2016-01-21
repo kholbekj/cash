86e4facd3b302279eb1515ba61defeb158b34743
Kasper Holbek Jensen <khj@autobutler.dk>
2016-01-21 17:03:51 +0100

    basic functionality

--- lib/cash.rb
+++ lib/cash.rb
@@ -1,5 +1,53 @@
-require "cash/version"
+class UnknownCurrencyError < StandardError; end
+class ExchangeRateZeroError < StandardError; end
 
-module Cash
-  # Your code goes here...
+class Cash
+  @_base_currency = nil
+  @_conversion_rates = {}
+
+  def self.conversion_rates(base, rates)
+    raise ExchangeRateZeroError if rates.values.include? 0
+    @_base_currency = String(base)
+    @_conversion_rates = Hash(rates)
+  end
+
+  def self.currencies
+    [@_base_currency, @_conversion_rates.keys].flatten
+  end
+
+  def self.get_base
+    @_base_currency
+  end
+
+  def self.get_rates
+    @_conversion_rates
+  end
+
+  def initialize(currency, amount)
+    raise UnknownCurrencyError unless Cash.currencies.include?(currency)
+
+    @currency = currency
+    @amount = amount
+  end
+
+  def convert_to(currency)
+    return Cash.new(currency, amount_in(currency))
+  end
+
+  private
+
+  def amount_in(currency)
+    raise UnknownCurrencyError unless Cash.currencies.include?(currency)
+    exchange_rate = Cash.get_rates[currency]
+    base_value * exchange_rate
+  end
+
+  def base_value
+    if Cash.get_base == @currency
+      @amount
+    else
+      own_rate = Cash.currencies[@currency]
+      amount / own_rate
+    end
+  end
 end
--- spec/cash_spec.rb
+++ spec/cash_spec.rb
@@ -1,36 +1,40 @@
 require 'spec_helper'
 
 describe Cash do
-  it 'has a version number' do
-    expect(Cash::VERSION).not_to be nil
-  end
-
   describe 'configuration' do
-    it 'can instantiate without setup' do
-      expect(Cash.new('EUR', 50)).not_to raise_error
+    it 'cannot instanciate non-exisitng currency' do
+      expect { Cash.new('EUR', 50) }.to raise_error(UnknownCurrencyError)
     end
 
-    it 'fails on convertion without rates' do
-      expect(Cash.new('EUR', 50).convert_to('USD')).to raise_error
+    it 'can instanciate a known currency' do
+      Cash.conversion_rates('EUR', {
+        'USD' => 1.11,
+        'Bitcoin' => 0.0047
+      })
+      expect(Cash.new('EUR', 50)).to be_a(Cash)
+    end
+
+    it 'fails if exchange rate is 0' do
+      expect { Cash.conversion_rates('USD', {'DKK' => 0})}
     end
   end
 
-  describe 'convertion' do
+  describe 'conversion' do
     let(:fifty_euro) {Cash.new('EUR', 50)}
 
     before do
-      Cash.convertion_rates('EUR', {
+      Cash.conversion_rates('EUR', {
         'USD' => 1.11,
         'Bitcoin' => 0.0047
       })
     end
 
     it 'converts to a different currency' do
-      expect(fifty_euro.convert_to('USD')).to be_a Cash
+      expect(fifty_euro.convert_to('USD')).to be_a(Cash)
     end
 
     it 'fails with a wrong currency' do
-      expect(fifty_euro.convert_to('Sausages')).to raise_error
+      expect { fifty_euro.convert_to('Sausages') }.to raise_error(UnknownCurrencyError)
     end
   end
 end
